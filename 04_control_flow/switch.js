d9fc80b8b233c4476a72e1b0320b7b474087e4e2
Snehal Prasad <85409845+Snehal-Prasad@users.noreply.github.com>
2024-08-16 13:06:26 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+//Syntax
+// switch (key){
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case"feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("April");
+        break;
+   
+
+    default:
+        console.log("default case match");
+        break;
+} 
